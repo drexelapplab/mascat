3aeb4419627a6583ccfdf8a0c9010269cd6bdee2
pequaad <noahknam@gmail.com>
2016-10-06 11:49:54 -0400

    gitignore added

--- mascat/bin/activate.fish
+++ mascat/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/applabadmin/Desktop/mascat/mascat"
+set -gx VIRTUAL_ENV "/Users/applabadmin/Google Drive/APP Lab/App Development/mascat/mascat"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
